1c5fb9e03d72d4cc413c357860073bc85b08009f
haianhltr <91930806+haianhltr@users.noreply.github.com>
2022-03-29 00:22:46 -0500

    Time: 3 ms (42.52%), Space: 47.1 MB (67.85%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -1,26 +1,4 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
+///DFS
 class Solution {
     public Node connect(Node root) {
         if(root == null) return null;
@@ -28,12 +6,14 @@ class Solution {
         queue.add(root);
         while(!queue.isEmpty())
         {
+            //create a node max to store the right-most node
             Node max = new Node(0);
             int size = queue.size();
             for(int k = 0; k  <size ; k++)
             {
                 
                 Node temp = queue.poll();
+                //since we starte from the right, the 
                 if(k==0){
                     temp.next = null;
                     max = temp;
@@ -44,10 +24,7 @@ class Solution {
                 }
                 if(temp.right != null) queue.add(temp.right);
                 if(temp.left != null) queue.add(temp.left);
-               
-               
-            }
-           
+            }        
         }
         return root;
     }
