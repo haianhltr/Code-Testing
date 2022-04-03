f5e0259b0e14200914d8badd829cf5c86d2909f9
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-04-02 23:20:06 -0500

    Time: 101 ms (100.00%), Space: 105.7 MB (100.00%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,33 +1,56 @@
 class Solution {
-   public List<List<Integer>> findWinners(int[][] matches) {
-        HashMap<Integer, Integer> def = new HashMap<>();
-        for(int[] arr : matches){
-            def.put(arr[1], def.getOrDefault(arr[1], 0) + 1);
+    public List<List<Integer>> findWinners(int[][] matches) {
+        List<List<Integer>> list = new ArrayList();
+        List<Integer> win = new ArrayList();
+        List<Integer> lost = new ArrayList();
+        Set<Integer> total = new HashSet();
+         Set<Integer> totallost = new HashSet();
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int [] temp : matches)
+        {
+            int a = temp[0];
+            int b = temp[1];
+            total.add(a);
+            total.add(b);
+            
         }
-        List<Integer> defeat = new ArrayList<>();
-        for(int val : def.keySet()){
-            if(def.get(val) == 1){
-                defeat.add(val);
-            }
-        }
-        Collections.sort(defeat);
-        HashMap<Integer, Integer> win = new HashMap<>();
-        for(int[] arr : matches){
-            win.put(arr[0], win.getOrDefault(arr[0], 0) + 1);
-        }
-        List<Integer> winner = new ArrayList<>();
         
-        for(int val : win.keySet()){
-            if(!def.containsKey(val)){
-                winner.add(val);
+     
+        for(int [] temp : matches)
+        {  
+            int a = temp[0];
+            int b = temp[1];
+            if(!map.containsKey(b) )
+            {
+                map.put(b,1);
             }
+            else{
+                int count = map.get(b);
+                map.put(b, count+1);
+            }
+          
+            total.remove(b);
+            
         }
-        Collections.sort(winner);
         
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(winner);
-        res.add(defeat);
-        return res;
-        //[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]
+          for (int ele : total) {
+            // Print HashSet data
+            win.add(ele);
+        }
+         Collections.sort(win);
+        
+        map.forEach((key,value) ->
+                    {
+                        
+                       if(value == 1) lost.add(key);
+                    });
+                    
+             Collections.sort(lost);
+        
+        
+        list.add(win);
+        list.add(lost);
+        return list;
+ 
     }
 }
