323116fcd92d2da30de4999bf760382c860a6d4e
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-04-05 18:19:47 -0500

    Time: 8 ms (54.89%), Space: 95.3 MB (26.82%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -18,6 +18,6 @@ class Solution {
         if(root == null) return 0;
         int left = minDepth(root.left);
         int right = minDepth(root.right);
-        return (root.left == null || root.right == null) ? left+right +1 : Math.min(left,right) +1;
+        return (root.left == null || root.right == null) ? left + right + 1 : Math.min(left,right) + 1;
     }
 }
