f41fc277a1c73ed3268e105607adaa5d075c492d
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-07-25 10:32:20 +0700

    Time: 4 ms (18.93%), Space: 54.3 MB (37.72%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -18,15 +18,12 @@ class Solution {
        {
            for(int j = 0; j < matrix[0].length; j++)
            {
-               if(column.contains(j))
+               if(column.contains(j) || row.contains(k))
                {
                    matrix[k][j] = 0;
                }
                
-                if(row.contains(k))
-               {
-                   matrix[k][j] = 0;
-               }
+               
            }
        }
         
@@ -37,24 +34,3 @@ class Solution {
     }
 }
 
-// 0 1
-// 2 1
-// 1 1
-
-// 1 
-    
-    
-// 0 0
-// 0 1 
-// 0 2
-
-
-// 0 0 
-// 0 1 *
-// 0 2
-// 1 0 **
-// 1 1 *
-// 1 2 **
-// 2 0 
-// 2 1 *
-// 2 2
