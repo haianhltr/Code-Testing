d56791000d51c9e939e1c6955ca5cbccf573a385
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-07-25 09:57:23 +0700

    Time: 6 ms (72.42%), Space: 75.6 MB (61.80%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int k = 0; k < nums.length; k++)
+        {
+            if(nums[Math.abs(nums[k])-1] > 0)
+            {
+                nums[Math.abs(nums[k])-1] *= -1;
+            }
+            else{
+                return Math.abs(nums[k]);
+            }
+        }
+        return -1;
+    }
+}
+
+
