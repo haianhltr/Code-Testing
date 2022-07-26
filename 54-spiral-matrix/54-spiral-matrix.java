d1a55fa32ad75ea4a89faf74dfd22645b9db0332
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-07-27 01:08:09 +0700

    Time: 0 ms (100.00%), Space: 42.2 MB (35.50%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -2,56 +2,43 @@ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new ArrayList();
         int top = 0;
-        int bottom = matrix.length;
-        int left = 0;
-        int right = matrix[0].length;
+        int bottom = matrix.length-1;
+        int left =0;
+        int right = matrix[0].length-1;
         
-        while(top < bottom && left < right){
-        for(int k = left ; k  < right; k++)
+        while(top <= bottom && left <= right )
         {
-            list.add(matrix[top][k]);
-        }
-        
-       top++;
-        
-        for(int k = top; k < bottom; k++)
-        {
-            list.add(matrix[k][right-1]);
-        }
-        
-        right--;
             
-               if(left >= right || top >= bottom) break;
-        
-        for(int k = right-1; k >= left; k-- )
-        {
-            list.add(matrix[bottom-1][k]);
+            for(int k = left; k <= right; k++)
+            {
+                list.add(matrix[top][k]);
+            }
+            top++;
+            for(int k = top; k <= bottom; k++)
+            {
+                list.add(matrix[k][right]);
+            }
+            right--;
+            
+            if(left > right || top > bottom) break;
+            for(int k = right; k >= left; k--)
+            {
+                list.add(matrix[bottom][k]);
+            }
+            bottom--;
+            
+            for(int k = bottom; k >= top; k--)
+            {
+                list.add(matrix[k][left]);
+            }
+            
+            left++;
+            
         }
-        
-        bottom--;
-      
-        
-        for(int k  = bottom-1; k >= top; k--)
-        {
-               list.add(matrix[k][left]);
-        }
-        
-        left++;
-        
-   
-        }
-        
-        
         return list;
-        
     }
 }
 
-
-// 00 01 02
-// 10 11 12
-// 20 21 22
-
-// 1 2  3  4
-// 5 6  7  8
-// 9 10 11 12
+// 00 01 02 03
+// 10 11 12 13
+// 20 21 22 23
