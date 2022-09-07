f6f73a7c7086301145b49e09cf8b586972356301
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-08 01:07:01 +0700

    Time: 19 ms (25.63%), Space: 54.8 MB (59.23%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -2,71 +2,65 @@ class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> list = new ArrayList();
         boolean [][] pacific = new boolean[heights.length][heights[0].length];
-        boolean [][] atlantic = new boolean [heights.length][heights[0].length];
-        Queue<int []> pacificQ = new LinkedList();
-        Queue<int []> atlanticQ = new LinkedList();
-        fill(pacificQ, pacific, 0,0);
-        BFS(pacificQ, pacific, heights);
-        fill(atlanticQ, atlantic,heights.length-1,heights[0].length-1);
-        BFS(atlanticQ,atlantic, heights);
+        boolean [][] atlantic = new boolean[heights.length][heights[0].length];
+        Queue<int []> queue = new LinkedList();
+        fill(pacific, queue, 0, 0);
+        BFS(pacific, queue, heights);
+        fill(atlantic, queue, heights.length-1, heights[0].length-1);
+        BFS(atlantic, queue, heights);
         
         for(int k = 0; k < heights.length; k++)
         {
             for(int j = 0; j < heights[0].length; j++)
             {
-                if((pacific[k][j] == true) && (atlantic[k][j] == true))
+                if(pacific[k][j] == true && atlantic[k][j] == true)
                 {
                     list.add(Arrays.asList(new Integer [] {k,j}));
                 }
             }
         }
-     
         return list;
+        
     }
     
-    public void fill(Queue<int []> queue, boolean [][] ocean, int maxTop, int maxSide)
+    public void fill(boolean [][] ocean, Queue<int []> queue, int top, int side)
     {
         for(int k = 0; k < ocean[0].length; k++)
         {
-            
-            queue.add(new int [] {maxTop,k});
-      
+            queue.add(new int [] {top,k});
         }
-        for(int k =0 ; k < ocean.length;k++)
+        for(int k = 0; k < ocean.length; k++)
         {
-      
-            queue.add(new int [] {k, maxSide});
-   
+            queue.add(new int [] {k,side});
         }
     }
-    public void BFS(Queue<int []> queue, boolean[][] oceanB,int [][] ocean)
+    
+    public void BFS(boolean[][] ocean, Queue<int []> queue, int [][] heights)
     {
-        while(!queue.isEmpty()){
-        int [] current = queue.poll();
-        int a= current[0];
-        int b = current[1];
-        if(oceanB[a][b] == true)
+        while(!queue.isEmpty())
         {
-            continue;
-        }
-        oceanB[a][b] = true;
-            
-        if(a+1 < ocean.length && ocean[a+1][b] >= ocean[a][b])
-        {
-            queue.add(new int [] {a+1, b});
-        }
-        if(a-1 >= 0 && ocean[a-1][b] >= ocean[a][b])
-        {
-            queue.add(new int []{a-1,b});
-        }
-        if(b-1 >= 0 && ocean[a][b-1] >= ocean[a][b])
-        {
-            queue.add(new int [] {a, b-1});
-        }
-        if(b+1 < ocean[0].length && ocean[a][b+1] >= ocean [a][b])
-        {
-            queue.add(new int [] {a, b+1});
-        }
+            int [] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            if(ocean[x][y] == true)
+            {continue;}
+            ocean[x][y] = true;
+            if(x + 1 < heights.length && heights[x+1][y] >= heights[x][y])
+            {
+                queue.add(new int [] {x+1, y});
+            }
+            if(x-1 >= 0 && heights[x-1][y] >= heights[x][y])
+            {
+                queue.add(new int [] {x-1, y});
+            }
+            if(y+1 < heights[0].length && heights[x][y+1] >= heights[x][y])
+            {
+                queue.add(new int [] {x,y+1});
+            }
+            if(y -1 >= 0 && heights[x][y-1] >= heights[x][y])
+            {
+                queue.add(new int [] {x,y-1});
+            }
         }
     }
 }
