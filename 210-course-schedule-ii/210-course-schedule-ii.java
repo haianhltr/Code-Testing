c374f410cc40346f09122a3bf23f602f46227e72
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-08 17:07:51 +0700

    Time: 3 ms (99.56%), Space: 43.6 MB (87.95%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,55 +1,52 @@
 class Solution {
     public int[] findOrder(int n, int[][] prerequisites) {
-           // what do i need, an array to store num of class need to be taken before take that class
-        int check = 0;
-       int [] result = new int [n]; 
-       int [] pre = new int [n];
-        // an array of class that the class can satisfy
-         ArrayList<Integer> [] catalog = new ArrayList[n];
-        //A queue to keep track
+
+        List<Integer> list = new ArrayList();
+        ArrayList<Integer> [] prereqs = new ArrayList[n];
+        int [] count = new int [n];
+        for(int k = 0 ; k < n; k++)
+        {
+            prereqs[k] = new ArrayList<Integer>();
+        }
+        
+        for(int [] current : prerequisites)
+        {
+            int a = current[0];
+            int b = current[1];
+            prereqs[b].add(a);
+            count[a]++;
+        }
+        
         Queue<Integer> queue = new LinkedList();
-        //boolean to return
-        
-        //since catalog is empty i need to generate object for it
-        for(int k =0; k < catalog.length; k++)
+        for(int k = 0; k < n; k++)
         {
-            catalog[k]=  new ArrayList<Integer>();
+            if(count[k] == 0)
+            queue.add(k);
         }
         
-        //now I need to put variables in catalog and prereqs
-        for(int [] edge : prerequisites)
-        {
-            int a = edge[0];
-            int b = edge[1];
-            catalog[b].add(a);
-            pre[a]++;
-        }
-        
-        for(int k = 0; k < n ; k++)
-        {
-            if(pre[k] == 0) 
-            {
-                queue.add(k);
-                // check = 1;
-            }
-        }
-        // if(check == 0) return new int[0];
-        // check = 0;
-        int k = 0;
+        //BFS
         while(!queue.isEmpty())
         {
             int current = queue.poll();
-            result[k++] = current;
+            list.add(current);
             n--;
-            for(int num : catalog[current])
+            for(int neighbor : prereqs[current])
             {
-                pre[num]--;
-                if(pre[num] == 0) queue.add(num);
+                count[neighbor]--;
+                if(count[neighbor] == 0)
+                {
+                    queue.add(neighbor);
+                }
                 
             }
         }
         
+        int [] result = new int [list.size()];
+        for(int k = 0; k < list.size(); k++)
+        {
+            result[k] = list.get(k);
+        }
+        return n == 0 ? result : new int [] {};
         
-        return n == 0 ? result : new int [0];
     }
 }
