3759563f80b028a62bad5c89d51430c477a8f782
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-10 00:39:26 +0700

    Time: 9 ms (53.59%), Space: 43.4 MB (94.75%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -6,60 +6,60 @@ class Solution {
         Queue<int []> queue = new LinkedList();
         fill(pacific, queue, 0, 0);
         BFS(pacific, queue, heights);
-        fill(atlantic, queue, heights.length-1, heights[0].length-1);
+        fill(atlantic, queue,heights.length-1, heights[0].length-1);
         BFS(atlantic, queue, heights);
         
         for(int k = 0; k < heights.length; k++)
         {
             for(int j = 0; j < heights[0].length; j++)
             {
-                if(pacific[k][j] == true && atlantic[k][j] == true)
+                if(atlantic[k][j] == true && pacific[k][j] == true)
                 {
                     list.add(Arrays.asList(new Integer [] {k,j}));
                 }
             }
         }
         return list;
-        
     }
-    
-    public void fill(boolean [][] ocean, Queue<int []> queue, int top, int side)
+    public void fill(boolean [][] ocean, Queue<int[]> queue, int topStart, int sideStart)
     {
-        for(int k = 0; k < ocean[0].length; k++)
-        {
-            queue.add(new int [] {top,k});
-        }
         for(int k = 0; k < ocean.length; k++)
         {
-            queue.add(new int [] {k,side});
+            queue.add(new int [] {k, sideStart});
         }
+        for(int k = 0; k < ocean[0].length; k++)
+        {
+            queue.add(new int [] {topStart,k});
+        }
+        
     }
-    
-    public void BFS(boolean[][] ocean, Queue<int []> queue, int [][] heights)
+    public void BFS(boolean [][] ocean, Queue<int []> queue, int[][] heights)
     {
         while(!queue.isEmpty())
         {
             int [] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
-            if(ocean[x][y] == true)
-            {continue;}
-            ocean[x][y] = true;
-            if(x + 1 < heights.length && heights[x+1][y] >= heights[x][y])
+            int a = current[0];
+            int b = current[1];
+            if(ocean[a][b] == true)
             {
-                queue.add(new int [] {x+1, y});
+                continue;
             }
-            if(x-1 >= 0 && heights[x-1][y] >= heights[x][y])
+            ocean[a][b] = true;
+            if(a+1 < heights.length && heights[a+1][b] >= heights[a][b])
             {
-                queue.add(new int [] {x-1, y});
+                queue.add(new int [] {a+1,b});
             }
-            if(y+1 < heights[0].length && heights[x][y+1] >= heights[x][y])
+            if(a-1 >= 0 && heights[a-1][b] >= heights[a][b])
             {
-                queue.add(new int [] {x,y+1});
+                queue.add(new int [] {a-1,b});
             }
-            if(y -1 >= 0 && heights[x][y-1] >= heights[x][y])
+            if(b+1 < heights[0].length && heights[a][b+1] >= heights[a][b])
             {
-                queue.add(new int [] {x,y-1});
+                queue.add(new int [] {a, b+1});
+            }
+            if(b-1 >= 0 && heights[a][b-1] >= heights[a][b])
+            {
+                queue.add(new int [] {a,b-1});
             }
         }
     }
