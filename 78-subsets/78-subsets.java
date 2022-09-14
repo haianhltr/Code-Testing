93ac2b06d77503491dd2bb4b440deb8bd2d12226
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-14 18:24:26 +0700

    Time: 1 ms (89.56%), Space: 43.2 MB (68.79%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,18 +1,27 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList();
-        Arrays.sort(nums);
-        backtrack(list, new ArrayList<>(), nums,0);
+         
+        recur(nums, 0, list, new ArrayList<>());
         return list;
     }
-    public void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int pos)
+    
+    public void recur(int [] nums, int pos, List<List<Integer>> list, List<Integer> temp)
     {
-        list.add(new ArrayList<>(tempList));
+     
+            list.add(new ArrayList<>(temp));
+      
+        
         for(int k = pos; k < nums.length; k++)
         {
-            tempList.add(nums[k]);
-            backtrack(list,tempList, nums, k+1);
-            tempList.remove(tempList.size()-1);
+            temp.add(nums[k]);
+            recur(nums, k+1, list, temp);
+            temp.remove(temp.size()-1);
+            
         }
+        
+    
     }
 }
+
+
