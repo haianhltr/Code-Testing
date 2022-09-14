a2cef9b99458c66d78cf5bd14e4e74dcc8c0c7df
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-14 19:42:38 +0700

    Time: 2 ms (39.42%), Space: 43.5 MB (33.72%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,27 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList();
-         
-        recur(nums, 0, list, new ArrayList<>());
-        return list;
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res,new ArrayList<>(),nums,0,false);
+        return res;
     }
     
-    public void recur(int [] nums, int pos, List<List<Integer>> list, List<Integer> temp)
-    {
-     
-            list.add(new ArrayList<>(temp));
-      
-        
-        for(int k = pos; k < nums.length; k++)
-        {
-            temp.add(nums[k]);
-            recur(nums, k+1, list, temp);
-            temp.remove(temp.size()-1);
-            
+    public void helper(List<List<Integer>> res, List<Integer> ls, int[] nums, int pos, boolean choosePre) {
+        if(pos==nums.length) {
+            res.add(new ArrayList<>(ls));
+            return;
         }
-        
-    
+        helper(res,ls,nums,pos+1,false);
+        ls.add(nums[pos]);
+        helper(res,ls,nums,pos+1,true);
+        ls.remove(ls.size()-1);
     }
 }
-
-
