cd9b61630098fd17c253a9734309e3176a8caf86
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-15 14:39:52 +0700

    Time: 122 ms (86.83%), Space: 40.4 MB (94.15%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,37 +1,27 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int k = 0; k < board.length;k++)
+        for(int k = 0; k < board.length; k++)
         {
             for(int j = 0; j < board[0].length;j++)
             {
-                if(board[k][j] == word.charAt(0) && DFS(board, k, j, 0, word))
-                {
+                if(word.charAt(0) == board[k][j] && backtrack(board, k,j, word, 0))
                     return true;
-                }
             }
         }
         return false;
-        
     }
-    
-    public boolean DFS(char [][] board, int k, int j, int pos, String word)
+    public boolean backtrack(char[][] board, int k, int j, String word, int pos)
     {
-        if(pos == word.length())
-        {
-            return true;
-        }
-        
-        if(k >= board.length || k < 0 || j >= board[0].length || j< 0 || board[k][j] != word.charAt(pos))
-        {
-            return false;
-        }
+        if(pos == word.length()) return true;
+        if(k < 0 || k >= board.length || j <0 || j >= board[0].length || word.charAt(pos) != board[k][j]) return false;
         
         
-       char temp =  board[k][j];
-        board[k][j] = ' ';
-        boolean result = DFS(board, k+1, j , pos+1, word) || DFS(board, k-1, j, pos+1, word) || DFS(board, k, j+1, pos+1, word) || DFS(board, k , j-1, pos+1, word);
+        char temp = board[k][j];
+        board[k][j]= ' ';
+        boolean result = backtrack(board, k+1, j, word, pos+1) || backtrack(board, k-1,j, word, pos+1) || backtrack(board,k, j+1, word, pos+1) || backtrack(board, k, j-1,word, pos+1);
         board[k][j] = temp;
         return result;
         
     }
 }
+
