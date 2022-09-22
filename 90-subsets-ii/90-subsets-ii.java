9ff21fb6e184d0f7a3b5e7d15189801cd2f6d025
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-22 20:22:11 +0700

    Time: 3 ms (50.61%), Space: 44.8 MB (9.53%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,37 +1,24 @@
 class Solution {
-    
-    Set<List<Integer>> set = new HashSet();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> list = new ArrayList();
+         List<List<Integer>> list = new ArrayList();
         Arrays.sort(nums);
-        recur(list,nums, 0, new ArrayList<>());
+        backtrack(nums, 0, list, new ArrayList<>(), new boolean[nums.length]);
         return list;
     }
     
-    
-    public void recur(List<List<Integer>> list, int [] nums, int pos, List<Integer> temp)
+    public void backtrack(int [] nums, int pos, List<List<Integer>> list, List<Integer> temp,boolean [] used)
     {
-        if(!set.contains(temp)){
-                set.add(temp);
-               list.add(new ArrayList<>(temp)); 
-        }
-        
-    
-        
-        for(int k = pos ; k < nums.length; k++)
+        list.add(new ArrayList<>(temp));
+        for(int k = pos; k < nums.length; k++)
         {
+            if(used[k] || k > pos && nums[k] == nums[k-1] && !used[k - 1]) continue;
+            used[k] = true; 
             temp.add(nums[k]);
-            recur(list, nums, k+1, temp);
-            temp.remove(temp.size() -1);
+            backtrack(nums, k+1, list, temp,used);
+                  used[k] = false; 
+            temp.remove(temp.size()-1);
+            }
+         
+            
         }
     }
-    
-}
-
-
-// [] [1] [1,2] [1,2,3] [2] [2,3]
-
-// 0 1
-// [1]
-
-// [2]
