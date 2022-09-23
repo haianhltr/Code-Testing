bf4d6b9a0676f8ce1e4b9978246220b498f72b47
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-24 00:43:18 +0700

    Time: 32 ms (61.41%), Space: 55.1 MB (40.98%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,27 +1,19 @@
 class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> list = new ArrayList();
-        backtrack(list, new ArrayList<>(), n, k , 1);
-        
-        
-        return list;
-    }
-    
-    public void backtrack(List<List<Integer>> list, List<Integer> temp, int n, int k, int pos)
-    {
-        if(temp.size() == k)
-        {
-            list.add(new ArrayList(temp));
-            return;
-        }
-        
-        for(int i = pos; i <= n; i++)
-        {
-            temp.add(i);
-            backtrack(list, temp, n, k, i+1);
-            temp.remove(temp.size()-1);
-        }
-        
-    }
+      public static List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> combs = new ArrayList<List<Integer>>();
+		combine(combs, new ArrayList<Integer>(), 1, n, k);
+		return combs;
+	}
+	public static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
+		if(k==0) {
+			combs.add(new ArrayList<Integer>(comb));
+			return;
+		}
+		for(int i=start;i<=n;i++) {
+			comb.add(i);
+			combine(combs, comb, i+1, n, k-1);
+			comb.remove(comb.size()-1);
+		}
+	}
     
 }
