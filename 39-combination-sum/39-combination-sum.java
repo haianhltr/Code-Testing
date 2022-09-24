447cf83c57051120d4e5df1f0c7c0b550dce8402
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-09-24 19:41:09 +0700

    Time: 8 ms (41.99%), Space: 44.3 MB (84.20%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -2,17 +2,19 @@ class Solution {
    public List<List<Integer>> combinationSum(int[] nums, int target) {
     List<List<Integer>> list = new ArrayList<>();
     Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
+ backtrack(list, new ArrayList(), nums, 0 , target, 0); 
     return list;
 }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int sum, int target, int pos){
+    if(sum > target) return;
+    else if(sum == target) list.add(new ArrayList<>(tempList));
     else{ 
-        for(int i = start; i < nums.length; i++){
+        for(int i = pos; i < nums.length; i++){
             tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+            sum += nums[i];
+            backtrack(list, tempList, nums, sum , target, i); 
+            sum -= nums[i];
             tempList.remove(tempList.size() - 1);
         }
     }
