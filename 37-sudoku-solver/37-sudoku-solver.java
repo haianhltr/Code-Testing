17efa341fbe63563b38de4f7c55b21bfc32f87a4
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-10 20:24:36 +0700

    Time: 33 ms (14.85%), Space: 41.6 MB (47.68%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,47 +1,42 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        if(board == null || board.length == 0)
-        {return;}
-        
+        if(board == null) return;
         backtrack(board);
     }
+           
     public boolean backtrack(char [][] board)
+    {
+            for(int k = 0; k < board.length; k++)
+            {
+                for(int j = 0; j < board[0].length; j++)
+                {
+                    if(board[k][j] == '.')
+                    {
+                        for(char c = '1'; c <= '9'; c++)
+                        {
+                            if(isValid(c, k,j, board))
+                            {
+                                board[k][j] = c;
+                                if(backtrack(board)) return true;
+                                else board[k][j] = '.';
+                             
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+        return true;
+    }
+           
+    public boolean isValid(char c, int row, int column, char [][] board)
     {
         for(int k = 0; k < board.length; k++)
         {
-            for(int j = 0; j < board[0].length;j++)
-            {
-                if(board[k][j] == '.')
-                {
-                    for(char c= '1'; c<= '9'; c++)
-                    {
-                        if(isValid(board, k,j, c))
-                        {
-                            board[k][j] = c;
-                            if(backtrack(board))
-                                return true;
-                            else
-                                board[k][j] = '.';
-                        }
-                    }
-                    return false;
-                }
-            }
+            if(board[k][column] != '.' && board[k][column] == c) return false;
+            if(board[row][k] != '.' && board[row][k] == c) return false;
+            if(board[3 * (row/3) + k/3][3 * (column/3) + k%3] != '.' && board[3 * (row/3) + k/3][3 * (column/3) + k%3] ==c) return false;
         }
-        return true;
-    }
-    
-    private boolean isValid(char[][] board, int row, int column, char c)
-    {
-        for(int i = 0 ; i < 9; i++)
-        {
-            if(board[i][column] != '.' && board[i][column] == c) return false;
-            if(board[row][i] != '.' && board[row][i] == c) return false;
-            if(board[3* (row/3) +i/3][3* (column/3) +i % 3] != '.' && 
-              board[3 * (row/3) + i / 3][3* (column/3) + i % 3] == c) return false; 
-                
-        }
-        
         return true;
     }
 }
