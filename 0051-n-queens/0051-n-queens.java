55b945a25fbed466537b32a92aec6e298c9f4928
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-11 12:21:00 +0700

    Time: 3 ms (92.61%), Space: 45.5 MB (51.47%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,56 +1,74 @@
 class Solution {
-  public List<List<String>> solveNQueens(int N) {
-	List<List<String>> list = new ArrayList<List<String>>();
-	if(N<1) return list;
-	char[][] board = new char[N][N];
-	  
-      //fill
-      for(int k = 0; k < board.length; k++)
-      {
-          for(int j = 0; j < board[0].length; j++)
-          {
-              board[k][j] = '.';
-          }
-      }
-      
-	solve(board, N, 0, list);
-	return list;
-}
-private boolean solve(char[][] board, int N, int count, List<List<String>> list) {
-	if(count==N) { // done soving, simply add the board into the result
-		List<String> temp = new ArrayList<String>();
-        for(int i = 0; i < N; i++) {
-            temp.add(String.valueOf(board[i]));
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> list = new ArrayList();
+        char [][] grid = new char [n][n];
+        for(int k = 0; k < n; k++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                grid[k][j] = '.';
+            }
         }
-		list.add(temp);
-		return false; // return false so will backtrack
-	}
+        backtrack(list, n, 0, grid);
+        return list;
+    }
     
     
+    public boolean backtrack(List<List<String>> list, int n, int column, char [][] grid)
+    {
+        if(column == n)
+        {
+            List<String> temp  = new ArrayList();
+            for(int k = 0; k < n; k++)
+            {
+                temp.add(String.valueOf(grid[k]));
+            }
+            list.add(temp);
+            return false;
+        }
+        
+        
+        for(int k = 0; k < n; k++)
+        {
+            if(isValid(grid, k, column, n))
+            {
+                grid[k][column] = 'Q';
+                if(backtrack(list, n, column +1, grid))
+                {
+                    return true;
+                }
+                else{
+                    grid[k][column] = '.';
+                }
+           
+        }
+    }
+        return false;
+    }
     
-	for(int i=0; i<N; i++) {
-		if(isValid(board, N, i, count)) {
-			board[i][count] = 'Q'; // greedy
-			if(solve(board, N, count+1, list)) return true;
-			else board[i][count] = '.'; // backtrack
-		}
-	}
-	return false;
-}
-    
-    
-private boolean isValid(char[][] board, int N, int row, int col) {
-	for(int i=0; i<N; i++) {
-		if(board[i][col]!='.') return false;
-		if(board[row][i]!='.') return false;
-	}
-	int step = 1;
-    //negative diagnal
-	while(row - step >=0 && col - step>=0)
-		if(board[row-step][col-step++]!='.') return false;
-	step = 1;
-	while(row + step <N && col -step>=0)
-		if(board[row+step][col-step++]!='.') return false;
-	return true;
-}
+    public boolean isValid(char [][] grid, int row, int column, int n)
+    {
+        for(int k = 0; k < n; k++)
+        {
+            if(grid[k][column] == 'Q') return false;
+            if(grid[row][k] == 'Q') return false;
+        }
+        
+        int step = 1;
+        
+        while(row - step >= 0 && column - step >= 0)
+        {
+            if(grid[row-step][column - step++] == 'Q') return false; 
+        }
+        
+        step  = 1;
+        while(row + step < n && column - step >= 0)
+        {
+            if(grid[row+step][column-step++] == 'Q') return false;
+        }
+        
+        return true;
+    }
+
+
 }
