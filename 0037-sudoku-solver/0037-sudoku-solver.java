d2c14bec2f50e96fff6f42565992c5561da73a49
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-13 23:08:47 +0700

    Time: 27 ms (25.81%), Space: 41.4 MB (68.47%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -5,42 +5,48 @@ class Solution {
     
     public boolean backtrack(char [][] board)
     {
-        
         for(int k = 0; k < board.length; k++)
         {
-            for(int j = 0; j < board[0].length; j++)
+            for(int j = 0; j < board[0].length;j++)
             {
                 if(board[k][j] == '.')
                 {
                     for(char c = '1'; c <= '9'; c++)
                     {
-                          if(isValid(board, k, j, c))
-                          {
-                              board[k][j] = c;
-                              if(backtrack(board))
-                              {return true;}
-                              else
-                              board[k][j] = '.';
-                                  
-                          }
+                        if(check(c, board, k, j) == true)
+                        {
+                            board[k][j] = c;
+                            if(backtrack(board))
+                            {
+                                return true;
+                            }
+                            else{
+                                board[k][j] = '.';
+                            }
+                            
+                        }
+                      
                     }
-                    return false;
-                  
+                      return false;
                 }
             }
         }
         return true;
+        
     }
     
-    public boolean isValid(char[][] board, int row, int column, char c)
+    
+
+    
+    public boolean check(char c, char [][] board, int row, int column)
     {
         for(int k = 0; k < board.length; k++)
         {
-            if(board[k][column] == c) return false;
-            if(board[row][k] == c) return false;
-            if(board[3* (row/3) + k/3][3 * (column/3) + k%3] == c) return false;
-           
+            if(board[k][column] != '.' && board[k][column] == c) return false;
+            if(board[row][k] != '.' && board[row][k] == c) return false;
+            if(board[3 * (row/3) + (k/3)][3 * (column/3) + (k%3)] != '.' && board[3 * (row/3) + (k/3)][3 * (column/3) + (k%3)] ==  c) return false;
         }
+        
         return true;
     }
 }
