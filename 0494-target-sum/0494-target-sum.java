a2d98cee4c99027854341adccc025b7474cfd7bb
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-18 21:07:18 +0700

    Time: 1265 ms (5.07%), Space: 42 MB (65.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,57 +1,24 @@
 class Solution {
     int count = 0;
-    
     public int findTargetSumWays(int[] nums, int target) {
-        DFS(nums, target, 0, 0);
+        DFS(nums, target,0, 0);
         return count;
     }
     
-    public void DFS(int [] nums, int target, int index, int sum)
+    public void DFS(int [] nums, int target, int sum, int index)
     {
         if(index == nums.length)
         {
-        if(sum == target)
-        {
+            if(sum == target)
+            {
+                count++;
+            }
+                            return;
 
-            count++;
-            
         }
-         return;
-        }
-   
         
-        DFS(nums, target, index + 1,  sum + nums[index]);
-        DFS(nums, target, index + 1, sum - nums[index]);
+        DFS(nums, target, sum + nums[index], index + 1);
+        DFS(nums,target, sum - nums[index], index + 1);
     }
+    
 }
-
-// first stack
-// 0
-// 0 + 1
-// ----
-// second stack
-// index = 1
-// 1 + 1
-// -----
-// i = 2
-// 2 + 1
-// ------
-// i = 3
-// 3 + 1
-// -------
-// i = 4
-// 4 + 1 
-// 4 - 1
-// ------
-// i = 5
-
-// -----
-// i = 5
-
-    
-    
-
-// // // if sum add up = target 
-// // // count++;
-// // // 1 + 1 + 1 + 1 +1
-
