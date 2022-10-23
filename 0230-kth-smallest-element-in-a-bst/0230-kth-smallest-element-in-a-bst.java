c4c39744b85262fb1ac50dc47566bec672042958
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-23 17:52:57 +0700

    Time: 0 ms (100.00%), Space: 42.2 MB (93.00%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,19 +14,21 @@
  * }
  */
 class Solution {
-   int count = 0;
-int result = Integer.MIN_VALUE;
-
+   public static int ans = 0;
 public int kthSmallest(TreeNode root, int k) {
-    traverse(root, k);
-    return result;
+    helper(root, k);
+    return ans;
 }
 
-public void traverse(TreeNode root, int k) {
-    if(root == null) return;
-    traverse(root.left, k);
-    count ++;
-    if(count == k) result = root.val;
-    traverse(root.right, k);       
+public int helper(TreeNode root, int k) {
+    if (root == null) {
+        return 0;
+    }
+    int leftCount = helper(root.left, k);
+    int rightCount = helper(root.right, k - leftCount - 1);
+    if (k == leftCount + 1) {
+        ans = root.val;
+    }
+    return leftCount + rightCount + 1;
 }
 }
