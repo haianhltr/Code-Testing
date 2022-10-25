81a12b840193ec3f5c97a02818bd91ac40ba4a6f
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-25 22:26:34 +0700

    Time: 14 ms (46.84%), Space: 47.4 MB (72.62%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,49 @@
-public class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList[] graph = new ArrayList[numCourses];
-        int[] degree = new int[numCourses];
-        Queue queue = new LinkedList();
-        int count=0;
-        
-        for(int i=0;i<numCourses;i++)
-            graph[i] = new ArrayList();
-            
-       for(int [] temp : prerequisites)
+class Solution {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        ArrayList<Integer> [] prereqs = new ArrayList[n];
+
+        int [] count = new int [n];
+        for(int k = 0; k < n; k++)
         {
-            int classCount = temp[0];
-            int classNeighbor = temp[1];
-             degree[classCount]++;
-            graph[classNeighbor].add(classCount);
+            prereqs[k] = new ArrayList<Integer>();
         }
         
+        for(int [] current : prerequisites)
+        {
+            int a  = current[0];
+            int b = current[1];
+            prereqs[b].add(a);
+            count[a]++;
+        }
         
-        
-        for(int i=0; i<degree.length;i++){
-            if(degree[i] == 0){
-                queue.add(i);
-                numCourses--;
+        Queue<Integer> queue = new LinkedList();
+        for(int k = 0; k < count.length; k++)
+        {
+            if(count[k] == 0)
+            {
+                queue.add(k);
+                n--;
             }
         }
         
-        while(queue.size() != 0){
-            int course = (int)queue.poll();
-            for(int i=0; i<graph[course].size();i++){
-                int pointer = (int)graph[course].get(i);
-                degree[pointer]--;
-                if(degree[pointer] == 0){
-                    queue.add(pointer);
-                   numCourses--;
+        while(!queue.isEmpty())
+        {
+            int current = queue.poll();
+   
+            for(int c : prereqs[current])
+            {
+                count[c]--;
+                if(count[c] == 0)
+                {
+                    queue.add(c);
+                             n--;
                 }
             }
         }
-        if(numCourses == 0)
-            return true;
-        else    
-            return false;
+        
+        return n==0;
+    
+        
+        
     }
 }
