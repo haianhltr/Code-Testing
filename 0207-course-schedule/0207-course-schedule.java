fb93fe6706127982b36fdb8639e01daf7efcb8da
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-26 23:35:47 +0700

    Time: 37 ms (11.50%), Space: 49.1 MB (28.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,49 +1,47 @@
 class Solution {
+         Set<Integer> set = new HashSet();
     public boolean canFinish(int n, int[][] prerequisites) {
-        ArrayList<Integer> [] prereqs = new ArrayList[n];
-
+ 
+        ArrayList<Integer>[] prereqs = new ArrayList[n];
         int [] count = new int [n];
         for(int k = 0; k < n; k++)
-        {
-            prereqs[k] = new ArrayList<Integer>();
-        }
+        {prereqs[k] = new ArrayList<Integer>();}
         
-        for(int [] current : prerequisites)
-        {
-            int a  = current[0];
-            int b = current[1];
-            prereqs[b].add(a);
-            count[a]++;
-        }
-        
-        Queue<Integer> queue = new LinkedList();
+        for(int [] pre : prerequisites)
+        {int a = pre[0];int b = pre[1];count[a]++;prereqs[b].add(a);}
         for(int k = 0; k < count.length; k++)
         {
             if(count[k] == 0)
             {
-                queue.add(k);
-               n--;
-            }
-        }
-        
-        while(!queue.isEmpty())
-        {
-            int current = queue.poll();
-   
-            for(int c : prereqs[current])
-            {
-                count[c]--;
-                if(count[c] == 0)
-                {
-                    queue.add(c);
-                             n--;
+                if(!set.contains(k))
+                { 
+                    n--;
+                       System.out.println("n" + n);
+                    n =  DFS(n, k, count, prereqs);   
                 }
+            System.out.println(n);
             }
         }
-        
-        return n==0;
-    
-        
-        
+        return n==0 ? true : false;
+    }
+    public int DFS(int n, int num, int [] count, ArrayList<Integer>[] prereqs)
+    {
+        set.add(num);
+        if(prereqs[num].size() == 0)
+        {
+
+            return n;
+        }
+        for(int a : prereqs[num])
+        {
+  
+            count[a]--;
+            if(count[a] == 0)
+            {
+                n--;
+                n = DFS(n, a ,count, prereqs);
+            }
+        }
+           return n;
     }
 }
