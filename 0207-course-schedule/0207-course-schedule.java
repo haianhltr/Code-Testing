0e4091786a2130b9ca5388ab048c8e0ef100ecc2
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-27 00:41:10 +0700

    Time: 7 ms (79.19%), Space: 46.9 MB (80.23%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,47 +1,48 @@
 class Solution {
-         Set<Integer> set = new HashSet();
+    Set<Integer> set = new HashSet();
     public boolean canFinish(int n, int[][] prerequisites) {
- 
         ArrayList<Integer>[] prereqs = new ArrayList[n];
         int [] count = new int [n];
-        for(int k = 0; k < n; k++)
-        {prereqs[k] = new ArrayList<Integer>();}
+        for(int k =0; k < n; k++)
+        {
+            prereqs[k] = new ArrayList();
+        }
         
-        for(int [] pre : prerequisites)
-        {int a = pre[0];int b = pre[1];count[a]++;prereqs[b].add(a);}
+        for(int [] temp : prerequisites)
+        {
+            count[temp[0]]++;
+            prereqs[temp[1]].add(temp[0]);
+        }
+        
         for(int k = 0; k < count.length; k++)
         {
             if(count[k] == 0)
             {
-                if(!set.contains(k))
-                { 
-                    n--;
-                       System.out.println("n" + n);
-                    n =  DFS(n, k, count, prereqs);   
+                if(!set.contains(k)){
+                n--;
+                n = DFS(prereqs, count, k, n);
                 }
-            System.out.println(n);
             }
         }
-        return n==0 ? true : false;
+        return n == 0;
     }
-    public int DFS(int n, int num, int [] count, ArrayList<Integer>[] prereqs)
+    public int DFS(ArrayList<Integer>[] prereqs, int [] count, int num, int n)
     {
         set.add(num);
         if(prereqs[num].size() == 0)
         {
-
             return n;
         }
+        
         for(int a : prereqs[num])
         {
-  
             count[a]--;
             if(count[a] == 0)
             {
                 n--;
-                n = DFS(n, a ,count, prereqs);
+                n = DFS(prereqs, count, a, n);
             }
         }
-           return n;
+        return n;
     }
 }
