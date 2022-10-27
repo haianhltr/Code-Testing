f54db89f5fc1456ec516510d943b8ce055514681
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-27 20:49:23 +0700

    Time: 12 ms (62.24%), Space: 49.6 MB (41.60%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,7 +1,10 @@
 class Solution {
+    Set<Integer>  set = new HashSet();
     public int[] findOrder(int n, int[][] prerequisites) {
-int [] result = new int [n];
+        int [] result = new int [n];
+        ArrayList<Integer> list = new ArrayList();
         ArrayList<Integer>[] prereqs = new ArrayList[n];
+     
         int [] count = new int [n];
         for(int k = 0; k < n; k++)
         {
@@ -11,38 +14,53 @@ int [] result = new int [n];
         
         for(int [] temp : prerequisites)
         {
-            int a = temp[0];
             count[temp[0]]++;
             prereqs[temp[1]].add(temp[0]);
-            
         }
         
-        Queue<Integer> queue = new LinkedList();
-        for(int k =0; k < count.length; k++)
+        for(int k = 0; k < count.length; k++)
         {
             if(count[k] == 0)
             {
-                queue.add(k);
-                n--;
-            }
-        }
-        int index = 0;
-        
-        while(!queue.isEmpty())
-        {
-            int temp = queue.poll();
-            result[index++] = temp;
-            for(int nei : prereqs[temp])
-            {
-                count[nei]--;
-                if(count[nei] == 0)
+                if(!set.contains(k))
                 {
-                    queue.add(nei);
-                    n--;
+                     n--;
+                   n = DFS(prereqs, count, n, k, list); 
                 }
+              
             }
         }
         
-        return n==0 ? result : new int [0];
+        for(int k = 0; k < count.length; k++)
+        {
+            if(list.size() == count.length)
+            result[k] = list.get(k);
+        }
+        
+        return (n == 0) ? result : new int[0];
+    }
+    
+    public int DFS(ArrayList<Integer>[] prereqs, int [] count, int n, int num, ArrayList<Integer> list)
+    {   
+        list.add(num);
+        set.add(num);
+      
+        if(prereqs[num].size() == 0)
+        {
+            return n;
+        }
+        
+        for(int a : prereqs[num])
+        {
+            count[a]--;
+            if(count[a] == 0)
+            {
+            
+                n--;
+            
+                n = DFS(prereqs, count, n, a, list);
+            }
+        }
+        return n;
     }
 }
