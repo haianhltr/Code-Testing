782bc39d0fd4a5ea89272ee4035fcccd5ba904be
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-30 00:03:11 +0700

    Time: 17 ms (75.69%), Space: 43.2 MB (39.06%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -1,49 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> list = new ArrayList();
+        if(k == 0)
+        {
+            list.add(target.val);
             
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        List<Integer> res = new LinkedList<>();
-        if (K == 0) {
-            res.add(target.val);
-        } else {
-            dfs(res, root, target.val, K ,0);
         }
-        return res;
+        else{
+            DFS(list, root, target, k,0);
+        }
+        
+        return list;
     }
+    public int DFS(List<Integer> list, TreeNode root, TreeNode target, int k, int depth)
+    {
+        //base case
+        if(root == null) return 0;
+        if(depth == k)
+        {
+            list.add(root.val);
+            return 0;
+        }
     
-    private int dfs(List<Integer> res, TreeNode node, int target, int K, int depth) {
-        if (node == null) return 0;
+        int left,right;    
+        //go left
+        if(root.val == target.val || depth > 0)
+        {
+            left = DFS(list, root.left, target, k, depth + 1);
+            right = DFS(list, root.right, target, k, depth + 1);
+        }
+        else
+        {
+            left = DFS(list, root.left, target, k, depth);
+            right = DFS(list, root.right, target, k, depth);
+        }
         
-        if (depth == K) {
-            res.add(node.val);
+        
+        if(root.val == target.val)
+        {
+            return 1;
+        }
+        
+        if(left == k || right == k)
+        {
+            list.add(root.val);
             return 0;
         }
-        
-        int left, right;
-        if (node.val == target || depth > 0) {
-            left = dfs(res, node.left, target, K, depth + 1);
-            right = dfs(res, node.right, target, K, depth + 1);
-        } else {
-            left = dfs(res, node.left, target, K, depth);
-            right = dfs(res, node.right, target, K, depth);
-        }
-        
-        if (node.val == target) return 1;
-        
-        if (left == K || right == K) {
-            res.add(node.val);
-            return 0;
-        }
-        
-        if (left > 0) {
-            dfs(res, node.right, target, K, left + 1);
+        //back to og
+        if(left > 0)
+        {
+            DFS(list, root.right, target, k, left + 1);
             return left + 1;
         }
-        
-        if (right > 0) {
-            dfs(res, node.left, target, K, right + 1);
+        if(right > 0)
+        {
+            DFS(list, root.left, target, k, right + 1);
             return right + 1;
         }
         
+     
+        
+        //return statement
         return 0;
     }
 }
