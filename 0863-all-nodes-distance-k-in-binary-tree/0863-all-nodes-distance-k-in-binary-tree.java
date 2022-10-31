6684330475cb10f2ffb85fc51b5dc19b43d8c27c
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-10-31 18:31:21 +0700

    Time: 25 ms (16.46%), Space: 43.2 MB (46.46%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -11,47 +11,47 @@ class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         List<Integer> list = new ArrayList();
         HashMap<TreeNode, Integer> hash = new HashMap();
-        fill(hash, target, root);
-        DFS(list, hash, target, root, 0, k);
+        fill(root, target, hash);
+        find(root, hash, k, 0, list);
         return list;
     }
-    public void DFS(List<Integer> list, HashMap<TreeNode, Integer> hash, TreeNode target, TreeNode root, int length, int k)
+    
+    public void find(TreeNode root, HashMap<TreeNode, Integer> hash, int k, int current, List<Integer> list)
     {
         if(root == null) return;
-        if(hash.containsKey(root)) length = hash.get(root);
-        if(k == length)
+        if(hash.containsKey(root)) current = hash.get(root);
+        if(k == current)
         {
-         list.add(root.val);
-         
+            list.add(root.val);
+    
         }
-        DFS(list, hash, target, root.left, length + 1, k);
-        DFS(list, hash, target, root.right, length + 1, k);
+        find(root.left, hash, k, current+1, list);
+        find(root.right, hash, k , current+1,list);
     }
     
-    public int fill(HashMap<TreeNode, Integer> hash, TreeNode target, TreeNode root)
+    public int fill(TreeNode root, TreeNode target, HashMap<TreeNode, Integer> hash)
     {
-        if(root == null)
-        {
-            return -1;
-        }
+        if(root == null) return -1;
         if(root.val == target.val)
         {
             hash.put(root, 0);
             return 0;
         }
         
-        int left = fill(hash, target, root.left);
-        if(left >= 0)
+        int a = fill(root.left, target, hash);
+        int b = fill(root.right, target, hash);
+        
+        if(a >= 0)
         {
-            hash.put(root, left + 1);
-            return left + 1;
+            hash.put(root, a + 1); 
+            return a+1;
         }
-        int right = fill(hash, target, root.right);
-        if(right >= 0)
-        {
-            hash.put(root, right + 1);
-            return right + 1;
-        }
+        if(b >= 0)
+        {hash.put(root, b + 1);
+            return b+1;}
+        
         return -1;
+        
+      
     }
 }
