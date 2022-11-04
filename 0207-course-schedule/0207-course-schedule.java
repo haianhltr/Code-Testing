4ef59a4948e1ad4370aa14853bbbd715f4a0c9fb
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-11-04 21:18:16 +0700

    Time: 8 ms (76.29%), Space: 47.4 MB (72.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,10 +1,9 @@
 class Solution {
     Set<Integer> set = new HashSet();
-
     public boolean canFinish(int n, int[][] prerequisites) {
-        ArrayList<Integer>[] prereqs = new ArrayList[n];
+        ArrayList<Integer> [] prereqs = new ArrayList[n];
         int [] count = new int [n];
-        for(int k = 0; k < n; k ++)
+        for(int k = 0; k < n; k++)
         {
             prereqs[k] = new ArrayList();
         }
@@ -15,40 +14,42 @@ class Solution {
             prereqs[temp[1]].add(temp[0]);
         }
         
+        
         for(int k = 0; k < count.length; k++)
         {
+            
             if(count[k] == 0)
             {
                 if(!set.contains(k))
-                n = DFS(prereqs, count, k, n);
-            
+                {
+                      n = DFS(prereqs, count, n, k);
+                }
+              
+                
             }
         }
-     
         
-        return n == 0 ? true : false;
+        return n== 0 ? true : false;
     }
     
-    public int DFS(ArrayList<Integer>[] prereqs,int [] count, int num, int n)
+    public int DFS(ArrayList<Integer> [] prereqs, int [] count, int n, int num)
     {
         set.add(num);
         n--;
-            
         if(prereqs[num].size() == 0)
         {
             return n;
         }
         
-        for(int a: prereqs[num])
+        for(int temp : prereqs[num])
         {
-            count[a]--;
-            if(count[a] == 0)
+            count[temp]--;
+            if(count[temp] == 0)
             {
-               n = DFS(prereqs, count ,a, n);
+                n = DFS(prereqs, count, n, temp);
             }
         }
         
-        System.out.println(n);
         return n;
     }
 }
