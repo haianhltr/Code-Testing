89acd6c3e735a6e8aab03cd8ea566a75a99c3f52
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-11-10 20:49:51 +0700

    Time: 3 ms (97.73%), Space: 42.7 MB (90.98%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,37 @@
 class Solution {
-        Set<Integer> set = new HashSet();
+    Set<Integer> set = new HashSet();
     public boolean canFinish(int n, int[][] prerequisites) {
-    
-        ArrayList<Integer>[] prereqs = new ArrayList[n];
+        ArrayList<Integer> [] prereqs = new ArrayList[n];
         int [] count = new int [n];
         for(int k = 0; k < n; k++)
         {
             prereqs[k] = new ArrayList();
-            
         }
         
-        for(int [] a : prerequisites)
+        for(int [] temp : prerequisites)
         {
-            prereqs[a[1]].add(a[0]);
-            count[a[0]]++;
+            prereqs[temp[1]].add(temp[0]);
+            count[temp[0]]++;
         }
         
-        for(int k = 0; k < count.length; k++)
+        for(int k =0; k < count.length; k++)
         {
             if(count[k] == 0)
             {
-               if(!set.contains(k))
-               n = DFS(prereqs, count, n, k);
+                if(!set.contains(k))
+                {
+                   n =  DFS(prereqs, count, n, k);
+                }
             }
         }
         
-        return n == 0 ? true : false;
+        return n == 0 ? true :false;
     }
     
-    
-    public int DFS(ArrayList<Integer>[] prereqs, int [] count, int n, int num)
+    public int DFS(ArrayList<Integer> [] prereqs, int [] count, int n, int num)
     {
-        set.add(num);
         n--;
+        set.add(num);
         if(prereqs[num].size() == 0)
         {
             return n;
@@ -48,5 +47,6 @@ class Solution {
         }
         
         return n;
+        
     }
 }
