df5043990673b8e2aae497571c37a8ba76d76e44
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-11-10 20:25:20 +0700

    Time: 15 ms (45.55%), Space: 54.9 MB (44.83%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -7,38 +7,38 @@ class Solution {
         boolean [][] atlantic = new boolean [rows][columns];
         for(int k = 0; k < rows; k++)
         {
-            DFS(pacific, heights, k, 0, rows, columns, heights[k][0]);
-            DFS(atlantic, heights, k, columns -1, rows, columns, heights[k][columns-1]);
+            //pacfic
+            DFS(pacific, heights, rows, columns, k, 0, heights[k][0]);
+            DFS(atlantic, heights, rows, columns, k, columns-1, heights[k][columns-1]);
         }
         for(int k = 0; k < columns; k++)
         {
-            DFS(pacific, heights, 0, k , rows, columns, heights[0][k]);
-            DFS(atlantic, heights, rows-1, k, rows, columns, heights[rows-1][k]);
+            DFS(pacific, heights, rows, columns, 0, k, heights[0][k]);
+            DFS(atlantic, heights, rows, columns, rows-1, k, heights[rows-1][k]);
         }
         
         for(int k = 0; k < rows; k++)
         {
             for(int j = 0; j < columns; j++)
             {
-                if(pacific[k][j] == true && atlantic[k][j] == true)
+                if(pacific[k][j] == true && atlantic [k][j] == true)
                 {
                     list.add(Arrays.asList(new Integer [] {k,j}));
                 }
             }
         }
         return list;
-        
     }
     
-    public void DFS(boolean [][] ocean, int [][] heights, int row, int column, int rows, int columns, int prev)
+    public void DFS(boolean [][] ocean, int [][] heights, int rows, int columns, int row, int column, int prevValue)
     {
-        if(row < 0 || row >= rows || column < 0 || column >= columns || ocean[row][column] == true || prev > heights[row][column])
+        if(row < 0 || row >= rows || column < 0 || column >= columns || prevValue > heights[row][column] || ocean[row][column] == true)
             return;
-        
         ocean[row][column] = true;
-        DFS(ocean, heights, row + 1, column, rows, columns, heights[row][column]);
-        DFS(ocean, heights, row - 1, column, rows, columns,  heights[row][column]);
-        DFS(ocean, heights, row, column - 1, rows, columns, heights[row][column]);
-        DFS(ocean, heights, row, column + 1, rows, columns, heights[row][column]);
+        DFS(ocean, heights, rows, columns, row + 1, column, heights[row][column]);
+        DFS(ocean, heights, rows, columns, row - 1, column,heights[row][column]);
+        DFS(ocean, heights, rows, columns, row, column + 1,heights[row][column]);
+        DFS(ocean, heights, rows, columns, row, column -1,heights[row][column]);
     }
+    
 }
