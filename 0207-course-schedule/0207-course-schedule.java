3e01c1b048b56ec2c550307fc487e51fd43d9091
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-11-29 17:49:51 +0700

    Time: 3 ms (97.51%), Space: 42.3 MB (96.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,13 +1,10 @@
-
 class Solution {
-    Set<Integer> set = new HashSet();
     public boolean canFinish(int n, int[][] prerequisites) {
-        ArrayList<Integer>[] prereqs = new ArrayList[n];
+        ArrayList<Integer> [] prereqs = new ArrayList[n];
         int [] count = new int [n];
-        for(int k= 0; k < n; k++)
+        for(int k = 0; k < n; k++)
         {
             prereqs[k] = new ArrayList();
-            
         }
         
         for(int [] temp : prerequisites)
@@ -16,37 +13,36 @@ class Solution {
             count[temp[0]]++;
         }
         
-        for(int k =0; k < count.length; k++)
+        boolean [] visited = new boolean[n];
+        
+        for(int k = 0; k < count.length; k++)
         {
             if(count[k] == 0)
             {
-                if(!set.contains(k))
-                {
-                   n = DFS(prereqs, count, k, n);
-                }
+                if(!visited[k])
+                n = DFS(n, prereqs, count, visited, k);
             }
         }
+        
+        System.out.println(n);
         return n == 0 ? true : false;
     }
     
-    public int DFS(ArrayList<Integer> [] prereqs, int [] count, int num, int n)
+    public int DFS(int n, ArrayList<Integer>[] prereqs, int [] count, boolean [] visited, int num)
     {
-        set.add(num);
         n--;
-        if(prereqs[num].size() == 0)
-        {
-            return n;
-        }
+        visited[num] = true;
+        if(prereqs[num].size() == 0) return n;
+        
         for(int nei : prereqs[num])
         {
             count[nei]--;
-            if(count[nei] == 0)
+            if(count[nei] == 0) 
             {
-               n = DFS(prereqs, count, nei, n);
+                if(!visited[nei])
+                n = DFS(n, prereqs, count, visited, nei);
             }
         }
         return n;
     }
-    
-    
 }
