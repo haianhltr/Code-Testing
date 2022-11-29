7748f2b3390db03f91fa146cb7147b157333d85d
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-11-29 17:21:02 +0700

    Time: 3 ms (97.35%), Space: 51 MB (86.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int count = 0;
-        for(int k = 0; k < grid.length; k++)
+        for(int k = 0; k < grid.length;k++)
         {
             for(int j = 0; j < grid[0].length; j++)
             {
                 if(grid[k][j] == '1')
                 {
-                    DFS(grid, k,j);
+                    DFS(grid, k, j);
                     count++;
                 }
             }
@@ -20,11 +20,11 @@ class Solution {
         {
             return;
         }
-        grid[k][j] = '0';
-        DFS(grid, k+1, j);
-        DFS(grid, k-1, j);
-        DFS(grid, k, j + 1);
-        DFS(grid, k, j -1);
         
+        grid[k][j] = '0';
+        DFS(grid, k + 1, j);
+        DFS(grid, k -1,j);
+        DFS(grid, k, j + 1);
+        DFS(grid, k, j - 1);
     }
 }
