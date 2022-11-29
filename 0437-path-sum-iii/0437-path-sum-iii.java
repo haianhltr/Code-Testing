118d0237205e9f06d0f26ab6ff0973de167e274e
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-11-29 18:43:07 +0700

    Time: 44 ms (20.49%), Space: 44 MB (57.74%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -1,23 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int count = 0;
+    int count  =0;
     public int pathSum(TreeNode root, int targetSum) {
         if(root == null) return 0;
         DFS(root, targetSum, 0);
+        
         pathSum(root.left, targetSum);
         pathSum(root.right, targetSum);
         return count;
@@ -25,9 +11,12 @@ class Solution {
     
     public void DFS(TreeNode root, int targetSum, long sum)
     {
-        if(root ==null) return;
-        sum+= root.val;
-        if(sum == targetSum) count++;
+        if(root == null) return;
+        sum += root.val;
+        if(sum == targetSum)
+        {
+            count++;
+        }
         DFS(root.left, targetSum, sum);
         DFS(root.right, targetSum, sum);
     }
