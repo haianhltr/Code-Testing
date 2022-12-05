b12d9b71ee45325a3aa0e63b5e1661924e0b346e
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-12-05 19:59:38 +0700

    Time: 0 ms (100.00%), Space: 39.3 MB (83.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,7 +8,8 @@ class Solution {
     }
     public int DFS(int n)
     {
-       if(n < 0) return 0;
+        if(n == 0) return 1;
+        if(n == 1) return 1;
       
         if(fib[n] != 0)
         {
