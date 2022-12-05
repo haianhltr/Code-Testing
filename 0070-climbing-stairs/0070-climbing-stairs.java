ae853e2db5bb7fcfcfbf8bbbe2ce993f332bd207
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-12-05 19:26:47 +0700

    Time: 2 ms (9.63%), Space: 42.7 MB (7.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,7 @@
 class Solution {
     int [] fib = new int[10000];
     public int climbStairs(int n) {
+        if(n < 0) return 0;
         if(n == 0) return 1;
         if(n == 1) return 1;
         if(fib[n] != 0)
