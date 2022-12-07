f7c5e96dc1cf362d62550cbc4fad2076863e1911
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-12-07 18:17:14 +0700

    Time: 4 ms (96.66%), Space: 43.8 MB (80.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,14 +1,11 @@
 class Solution {
-    Set<Integer> set = new HashSet();
+    List<Integer> list = new ArrayList();
     public int[] findOrder(int n, int[][] prerequisites) {
-        ArrayList<Integer>[] prereqs= new ArrayList[n];
+        ArrayList<Integer> [] prereqs = new ArrayList[n];
         int [] count = new int [n];
-        int [] result = new int[n];
-        List<Integer> list = new ArrayList();
         for(int k = 0; k < n; k++)
         {
             prereqs[k] = new ArrayList();
-            
         }
         
         for(int [] temp : prerequisites)
@@ -17,49 +14,35 @@ class Solution {
             count[temp[0]]++;
         }
         
+        boolean [] visited = new boolean[n];
+        
         for(int k = 0; k < count.length; k++)
         {
             if(count[k] == 0)
             {
-                if(!set.contains(k))
-                {
-                    n = DFS(prereqs, count, list, k, n);
-                }
+                if(visited[k] == false)
+                n = DFS(prereqs, count, visited, n, k);
             }
         }
-        
-        if(n == 0){
-            
-        
-        for(int k = 0; k < result.length; k++)
-        {
-            result[k] = list.get(k);
-        }
-        }
+
+       int[] result = list.stream().mapToInt(Integer::intValue).toArray();
         
         return n == 0 ? result : new int [] {};
-            
     }
-    
-    public int DFS(ArrayList<Integer>[] prereqs, int [] count, List<Integer> list, int num, int n)
+    public int DFS(ArrayList<Integer> [] prereqs, int [] count, boolean [] visited, int n, int num)
     {
-        set.add(num);
+        visited[num] = true;
         n--;
         list.add(num);
-        if(prereqs[num].size() == 0)
-        {
-            return n;
-        }
-        
+        if(prereqs[num].size()== 0) return n;
         for(int nei : prereqs[num])
         {
             count[nei]--;
             if(count[nei] == 0)
             {
-                n = DFS(prereqs, count, list, nei, n);
+                n = DFS(prereqs, count, visited, n, nei);
             }
-         }
-        
+        }
         return n;
     }
 }
