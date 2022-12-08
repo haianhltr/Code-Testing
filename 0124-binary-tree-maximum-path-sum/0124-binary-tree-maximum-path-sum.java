1ece2720d1be12be16329bce1eaa4aa3501baf3f
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-12-08 20:48:40 +0700

    Time: 1 ms (99.60%), Space: 44.1 MB (83.21%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,18 +14,18 @@
  * }
  */
 class Solution {
-    int num = Integer.MIN_VALUE;
+    int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-      DFS(root);
-        return num;
+          DFS(root);
+        return max;
     }
     
     public int DFS(TreeNode root)
     {
         if(root == null) return 0;
         int a = Math.max(0, DFS(root.left));
-        int b = Math.max(0, DFS(root.right));
-        num = Math.max(num, root.val + a + b);
+        int b= Math.max(0, DFS(root.right));
+        max = Math.max(max, root.val+ a  + b);
         return Math.max(a,b) + root.val;
     }
 }
