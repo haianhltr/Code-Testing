31d21194670273520515bd63a686daea292fb2ec
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-12-10 14:00:04 +0700

    Time: 192 ms (67.85%), Space: 42.6 MB (90.76%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,59 +1,59 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> list = new ArrayList();
-        TrieNode root = buildTrie(words);
+        TrieNode p = new TrieNode();
+        p = buildTrie(words);
         for(int k = 0; k < board.length; k++)
         {
             for(int j = 0; j < board[0].length; j++)
             {
-                DFS(board, words, k, j, root, list);
+                DFS(board, words, k, j,p , list);
             }
         }
         return list;
     }
     
-    
     public void DFS(char [][] board, String [] words, int row, int column, TrieNode p, List<String> list)
     {
-        if(row < 0 || row >= board.length || column < 0 || column >= board[0].length || board[row][column] == '#' || p.next[board[row][column] - 'a'] == null )
-        {
-            return;
-        }
+        //base case
+        if(row < 0 || row >= board.length || column < 0 || column >= board[0].length || board[row][column] == '#' || p.next[board[row][column] - 'a'] == null) return;
         
         char letter = board[row][column];
-        p = p.next[letter -'a'];
+        p = p.next[letter - 'a'];
         if(p.word != null)
         {
             list.add(p.word);
             p.word = null;
         }
+        
         board[row][column] = '#';
         DFS(board, words, row + 1, column, p, list);
-        DFS(board,words, row - 1, column, p, list);
+        DFS(board, words, row -1, column, p, list);
         DFS(board, words, row, column + 1, p, list);
         DFS(board, words, row, column - 1, p, list);
         board[row][column] = letter;
+        
     }
     
     public TrieNode buildTrie(String [] words)
     {
         TrieNode root = new TrieNode();
-        for(String w : words)
+        for(String each : words)
         {
             TrieNode temp = root;
-            for(char c : w.toCharArray())
+            for(char c : each.toCharArray())
             {
                 int i = c - 'a';
                 if(temp.next[i] == null) temp.next[i] = new TrieNode();
                 temp = temp.next[i];
             }
-            temp.word = w;
+            temp.word = each;
         }
-        
         return root;
     }
     
-    class TrieNode
+    
+    public class TrieNode
     {
         TrieNode [] next = new TrieNode[26];
         String word;
