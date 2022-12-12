eb51955ab67050e28b5b9cc3d74a6f94955229df
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-12-12 09:56:06 +0700

    Time: 192 ms (67.62%), Space: 42.4 MB (92.88%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> list = new ArrayList();
-        TrieNode p = new TrieNode();
-        p = buildTrie(words);
+        TrieNode root = new TrieNode();
+        root = fillTrie(words);
         for(int k = 0; k < board.length; k++)
         {
             for(int j = 0; j < board[0].length; j++)
             {
-                DFS(board, words, k, j,p , list);
+                DFS(board, words, k, j, root, list);
             }
         }
+        
         return list;
     }
     
     public void DFS(char [][] board, String [] words, int row, int column, TrieNode p, List<String> list)
     {
-        //base case
-        if(row < 0 || row >= board.length || column < 0 || column >= board[0].length || board[row][column] == '#' || p.next[board[row][column] - 'a'] == null) return;
-        
+        if(row < 0 || row >= board.length || column < 0 || column >= board[0].length || board[row][column] == '#' ||  p.next[board[row][column] - 'a'] == null)
+            return;
+            
+     
         char letter = board[row][column];
         p = p.next[letter - 'a'];
         if(p.word != null)
@@ -26,28 +28,31 @@ class Solution {
             p.word = null;
         }
         
-        board[row][column] = '#';
+               board[row][column] = '#';
         DFS(board, words, row + 1, column, p, list);
-        DFS(board, words, row -1, column, p, list);
-        DFS(board, words, row, column + 1, p, list);
-        DFS(board, words, row, column - 1, p, list);
+         DFS(board, words, row - 1, column, p, list);
+         DFS(board, words, row, column + 1, p, list);
+         DFS(board, words, row, column - 1, p, list);
         board[row][column] = letter;
         
     }
     
-    public TrieNode buildTrie(String [] words)
+    
+    public TrieNode fillTrie(String [] words)
     {
         TrieNode root = new TrieNode();
-        for(String each : words)
+        for(String word : words)
         {
             TrieNode temp = root;
-            for(char c : each.toCharArray())
+            for(char c : word.toCharArray())
             {
                 int i = c - 'a';
                 if(temp.next[i] == null) temp.next[i] = new TrieNode();
                 temp = temp.next[i];
+                 
             }
-            temp.word = each;
+            
+            temp.word = word;
         }
         return root;
     }
