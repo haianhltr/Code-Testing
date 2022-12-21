71f52695e95c6e3b51c06e83353e5719f281f5bd
Anh Le <91930806+haianhltr@users.noreply.github.com>
2022-12-21 13:27:01 +0700

    Time: 2 ms (79.83%), Space: 44.7 MB (50.95%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -16,24 +16,22 @@
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> list = new ArrayList();
-        DFS(root, targetSum, new ArrayList<Integer>(), list);
+        DFS(root, targetSum, list, new ArrayList());
         return list;
     }
-    
-    public void DFS(TreeNode root, int targetSum, List<Integer> temp, List<List<Integer>> list)
+    public void DFS(TreeNode root, int targetSum,  List<List<Integer>> list, List<Integer> temp)
     {
-     
         if(root == null) return;
         temp.add(root.val);
-        if(root.left == null && root.right == null && targetSum - root.val == 0) 
+        if(root.left == null && root.right == null && targetSum - root.val ==0)
         {
-           list.add(new ArrayList(temp));
-           temp.remove(temp.size() -1);
-           return;
+            list.add(new ArrayList(temp));
+            temp.remove(temp.size() -1);
+            return;
         }
         
-        DFS(root.left, targetSum - root.val, temp, list);
-        DFS(root.right, targetSum - root.val, temp,  list);
-        temp.remove(temp.size() -1);
+        DFS(root.left, targetSum - root.val, list, temp);
+        DFS(root.right, targetSum - root.val, list, temp);
+        temp.remove(temp.size()-1);
     }
 }
