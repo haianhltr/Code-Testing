0c5482b11e5fe26d63954e3dc3df2a651e389b79
Anh Le <91930806+haianhltr@users.noreply.github.com>
2023-01-04 20:29:21 +0700

    Time: 0 ms (100.00%), Space: 39.9 MB (72.56%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -10,21 +10,44 @@ class Solution {
             arr1[k] = nums[k];
             arr2[k] = nums[k+1];
         }
-        
+        return Math.max(DP(arr1), DP(arr2));
      
         
-        int [] dp1 = new int[len - 1];
-        int [] dp2 = new int[len - 1];
-        for(int k = 0; k < dp1.length; k++)
+//         int [] dp1 = new int[len - 1];
+//         int [] dp2 = new int[len - 1];
+//         for(int k = 0; k < dp1.length; k++)
+//         {
+//             dp1[k] = -1;
+//             dp2[k] = -1;
+//         }
+        
+//         int ans1 = calc(arr1, len - 2, dp1);
+//         int ans2 = calc(arr2, len -2, dp2);
+//         return Math.max(ans1, ans2);
+    }
+    
+    public int DP(int [] nums)
+    {
+        int len = nums.length;
+        int [] dp = new int[len];
+        dp[0] = nums[0];
+        for(int k = 1; k < nums.length; k++)
         {
-            dp1[k] = -1;
-            dp2[k] = -1;
+            int include = nums[k];
+            if(k > 1)
+            {
+                include += dp[k-2];
+            }
+            
+            int exclude = dp[k-1];
+            dp[k] = Math.max(include, exclude);
         }
         
-        int ans1 = calc(arr1, len - 2, dp1);
-        int ans2 = calc(arr2, len -2, dp2);
-        return Math.max(ans1, ans2);
+        return dp[len-1];
     }
+    
+
+    
     
     public int calc(int [] nums, int index, int [] dp)
     {
@@ -49,6 +72,5 @@ class Solution {
     
 
 
-    
     
 }
