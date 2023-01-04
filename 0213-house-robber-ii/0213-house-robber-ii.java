5fc98e13172ef554bf3165dca6647743d54a3f26
Anh Le <91930806+haianhltr@users.noreply.github.com>
2023-01-04 20:06:19 +0700

    Time: 1 ms (41.98%), Space: 41.5 MB (40.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,48 +1,56 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return nums[0];
-        int [] arr1 = new int[n-1];
-        int [] arr2 = new int[n-1];
-        for(int k =0; k < n-1; k++)
+        int len = nums.length;
+        if(len == 1) return nums[0];
+        int [] arr1 = new int[len-1];
+        int []  arr2= new int[len -1];
+        for(int k =0; k < len - 1; k++)
         {
-            arr1[k] = nums[k]; //include first but not last element
-            arr2[k] = nums[k+1]; //not include first but last element;
-        }
-  
-
-        //top down
-        int x = arr1.length-1;
-        int [] dp1 = new int[x + 1];
-        int [] dp2 = new int[x + 1];
-        for(int k = 0; k < x + 1; k++)
-        {
-            dp1[k] = -1;
-            dp2[k] = -1;
-        }
+            arr1[k] = nums[k];
+            arr2[k] = nums[k+1];
+            }
+            
         
-        int ans1 = calc(arr1, x, dp1);
-        int ans2 = calc(arr2, x, dp2);
-        
-        return Math.max(ans1, ans2);
+        return Math.max(DP(arr1), DP(arr2));
         
     }
- 
-    public int calc(int [] nums, int index, int [] dp)
+    
+    //bottom up
+    public int DP(int [] nums)
     {
-        if(index == 0) return nums[0];
-        if(index < 0) return 0;
-        if(dp[index] != -1)
+        int len = nums.length;
+        int [] dp = new int[len];
+        dp[0]  = nums[0];
+        for(int k = 1; k < len; k++)
         {
-            return dp[index];
+            int include = nums[k];
+            if(k > 1)
+            {
+                include = include + dp[k-2]; 
+            }
+            int exclude = dp[k-1];
+            dp[k] = Math.max(include, exclude);
         }
-        
-        int include = calc(nums, index - 2, dp) + nums[index];
-        int exclude = calc(nums, index-1, dp);
-        return dp[index] = Math.max(include, exclude);
+        return dp[len-1];
     }
 }
 
 
+// 0 1 2
+// 1 2 3
+
+// 1 2 4
 
 
+//     0 1 2
+//     1 2 3
+    
+// dp  0 1 2
+//     1 2
+//     k = 1
+//     include =2
+//     exclude = 1
+    
+//     k = 2
+//     include = 3 => include = 3 + 1 = 4
+//     exclude = 2 => exclude = 2
