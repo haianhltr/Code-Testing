79aaa1ef5ccb0c15d3a6fcd534cbcf90b2b52fe8
Anh Le <91930806+haianhltr@users.noreply.github.com>
2023-01-05 20:15:59 +0700

    Time: 13 ms (98.51%), Space: 42.1 MB (83.23%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int [] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for(int k = 1; k <= amount; k++)
+        {
+            for(int c : coins)
+            {
+                if(k - c >= 0)
+                {
+                    dp[k] = Math.min(dp[k], 1 + dp[k -c]);
+                }
+            }
+        }
+        return dp[amount] != amount + 1 ? dp[amount] : -1;
+    }
+}
