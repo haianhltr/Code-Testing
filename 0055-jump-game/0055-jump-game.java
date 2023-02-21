dc97e9f665043aa6fa323624f28aed2fb84d31dc
Anh Le <91930806+haianhltr@users.noreply.github.com>
2023-02-20 21:51:20 -0600

    Time: 381 ms (12.99%), Space: 43.3 MB (12.27%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,29 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int lastPos = nums.length-1;
-        for(int k = nums.length - 1; k >= 0; k--)
-        {
-            if(k + nums[k] >= lastPos)
-            lastPos = k;
-        }
-        return lastPos == 0;
+        Boolean[] dp = new Boolean[nums.length];
+        return DFS(nums, 0, dp);
     }
+    
+    private boolean DFS(int[] nums, int index, Boolean[] dp) {
+        if (index >= nums.length - 1) {
+            return true;
+        }
+        
+        if (dp[index] != null) {
+            return dp[index];
+        }
+        
+        int furthestJump = Math.min(index + nums[index], nums.length - 1);
+        for (int i = index + 1; i <= furthestJump; i++) {
+            if (DFS(nums, i, dp)) {
+                dp[index] = true;
+                return true;
+            }
+        }
+        
+        dp[index] = false;
+        return false;
+    
+}
+
 }
